7fbec3c67cfb0543062f1724aede9e42f7702933
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-05-27 21:08:40 +0300

    footer

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,6 +1,7 @@
 import { Text } from "@/shared/ui/Text";
 import cls from "./Footer.module.scss";
 import { classNames } from "@/shared/lib/classNames/classNames";
+import { NavLink } from "react-router-dom";
 
 interface FooterProps {
     noMargin?: boolean;
@@ -17,15 +18,29 @@ export const Footer = ({ noMargin, className }: FooterProps) => {
                         FLAVORS
                     </Text>
                     <div className={cls.right}>
-                        <Text color="white" textType="text" tagType="p">
-                            +7 999 999 99 99
-                        </Text>
-                        <Text color="white" textType="text" tagType="p">
-                            наб. реки Мойки 84
-                        </Text>
-                        <Text color="white" textType="text" tagType="p">
-                            addisflavorsfeedback@gmail.com
-                        </Text>
+                        <div>
+                            <NavLink to={"/delivery-info"}>
+                                <Text color="white" textType="text" tagType="p">
+                                    Условия доставки
+                                </Text>
+                            </NavLink>
+                            <NavLink to={"/about"}>
+                                <Text color="white" textType="text" tagType="p">
+                                    О нас
+                                </Text>
+                            </NavLink>
+                        </div>
+                        <div>
+                            <Text color="white" textType="text" tagType="p">
+                                +7 999 999 99 99
+                            </Text>
+                            <Text color="white" textType="text" tagType="p">
+                                наб. реки Мойки 84
+                            </Text>
+                            <Text color="white" textType="text" tagType="p">
+                                addisflavorsfeedback@gmail.com
+                            </Text>
+                        </div>
                     </div>
                 </div>
             </div>
